c45859a85a40641b5f4d5771a495d786b95cf4a5
zhouchuang <zhouchuang1217@163.com>
2018-05-10 23:37:13 +0800

    第二次提交

---
+++ src/main/java/controller/MybatisController.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * mybatis生成控制层
+ *
+ * @author zhouchuang
+ * @create 2018-05-10 23:36
+ */
+public class MybatisController {
+}
---
+++ src/main/java/controller/TableController.java
@@ -0,0 +1,10 @@
+package controller;
+
+/**
+ * 表设计控制层
+ *
+ * @author zhouchuang
+ * @create 2018-05-10 23:35
+ */
+public class TableController {
+}
