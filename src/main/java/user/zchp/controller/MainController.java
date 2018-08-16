a7870e4641d847c999604548d71bbd3aca8451d0
zhouchuang <zhouchuang1217@163.com>
2018-08-16 21:20:28 +0800

    Merge branch 'dev'
    # Conflicts:
    #	src/main/java/user/zchp/general/pipeline/Console.java

---
+++ src/main/java/user/zchp/controller/MainController.java
@@ -0,0 +1,67 @@
+package user.zchp.controller;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import user.zchp.models.base.User;
+import user.zchp.service.base.PermissionService;
+import user.zchp.service.base.RoleService;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 主要的控制层
+ *
+ * @author zhouchuang
+ * @create 2018-05-12 10:07
+ */
+@Controller
+@RequestMapping("")
+public class MainController {
+
+    @Autowired
+    PermissionService permissionService;
+    @Autowired
+    RoleService roleService;
+
+    @RequestMapping(value="/indexMain")
+    public String indexMain(Model model , HttpServletRequest request) {
+        return "/index";
+    }
+
+    @RequestMapping(value="/loginMain")
+    public String loginMain(Model model , HttpServletRequest request) {
+        return "/login";
+    }
+
+    /**
+     *登录
+     */
+    @RequestMapping("/toLogin")
+    public String toLogin(Model model ,User user){
+        UsernamePasswordToken token = new UsernamePasswordToken(user.getAccountNo(),user.getPassword());
+        Subject subject = SecurityUtils.getSubject();
+        token.setRememberMe(true);
+        try {
+            subject.login(token);
+            subject.getSession().setAttribute("user",user);
+            subject.getSession().setAttribute("menus",permissionService.findUserPermissionList(roleService.findRoleIdByAccountNo(user.getAccountNo())));
+            return "redirect:/indexMain";
+        } catch (Exception e) {
+            e.printStackTrace();
+            model.addAttribute("user", user);
+            model.addAttribute("result","用户名或密码错误");
+            return "redirect:/loginMain";
+        }
+    }
+
+    @RequestMapping(value="/forward",method= RequestMethod.GET, produces = {"text/html; charset=utf-8"})
+    public String forward(String path){
+        return path;
+    }
+}
---
+++ src/main/java/user/zchp/controller/TableController.java
@@ -0,0 +1,61 @@
+package user.zchp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import user.zchp.models.LeftTable;
+import user.zchp.service.base.TableService;
+import user.zchp.utils.Result;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 表设计控制层
+ *
+ * @author zhouchuang
+ * @create 2018-05-10 23:35
+ */
+@Controller
+@RequestMapping("/TableController")
+public class TableController {
+
+    @Autowired
+    TableService tableService;
+
+    @RequestMapping(value="/index")
+    public String index(Model model , HttpServletRequest request) {
+        return "/table/index";
+    }
+
+    @RequestMapping(value = "/TableList")
+    @ResponseBody
+    public Result tableList(String database ){
+        Result result = new Result();
+        List<String> list = tableService.tableList(database);
+        result.setData(list);
+        return result;
+    }
+
+    @RequestMapping(value = "/FieldList")
+    @ResponseBody
+    public Result fieldList(String table ){
+        Result result = new Result();
+        List<Map> list = tableService.fieldList(table);
+        result.setData(list);
+        return result;
+    }
+
+    @RequestMapping(value = "/generalDao", method = RequestMethod.POST, produces="application/json")
+    @ResponseBody
+    public Result generalDao(@RequestBody LeftTable leftTable){
+        Result result = new Result();
+        return result;
+    }
+
+}
