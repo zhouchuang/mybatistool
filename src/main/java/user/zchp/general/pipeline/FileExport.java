05439ebba38324222a2eaba54add5e122121132f
zhouchuang <zhouchuang1217@163.com>
2018-08-24 15:54:15 +0800

    修改数据库为远程数据库

--- src/main/java/user/zchp/general/pipeline/FileExport.java
+++ src/main/java/user/zchp/general/pipeline/FileExport.java
@@ -8,7 +8,7 @@ package user.zchp.general.pipeline;
  */
 public class FileExport implements Pipeline{
     @Override
-    public void process() {
+    public void process(StringBuffer stringBuffer) {
 
     }
 }
