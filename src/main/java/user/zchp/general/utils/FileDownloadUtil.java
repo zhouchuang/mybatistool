1bc5367ec636851e7046606ea6e2976de440f4ed
zhouchuang <zhouchuang1217@163.com>
2018-10-12 09:02:25 +0800

    下载压缩文件

--- src/main/java/user/zchp/general/utils/FileDownloadUtil.java
+++ src/main/java/user/zchp/general/utils/FileDownloadUtil.java
@@ -5,7 +5,6 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import user.zchp.general.component.Table;
 import user.zchp.general.component.TemplateInfo;
 
 import java.io.*;
